37c7e689864fbd51cd9f60635a85f327ade885ee
Michel Morais <valentimmichel@gmail.com>
2023-03-25 09:03:47 -0300

    HashCode

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,7 +7,11 @@ public class Program {
 		String a = "Maria";
 		String b = "Alex";
 		
-		System.out.println(a.equals(b));
+		System.out.println(a.hashCode());
+		System.out.println(b.hashCode());
+		/*Regra de ouro do HashCode
+		Se o hashCode de dois objetos for diferente, 
+		então os dois objetos são diferentes*/
 	}
 
 }
